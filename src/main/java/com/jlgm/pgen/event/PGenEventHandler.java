d7f2d266c57ba5f53ea397efe16e38e8abb8bb83
JoseluGames <joselugamesofficial@gmail.com>
2016-09-29 10:36:34 +0200

    Fix update link

--- src/main/java/com/jlgm/pgen/event/PGenEventHandler.java
+++ src/main/java/com/jlgm/pgen/event/PGenEventHandler.java
@@ -15,7 +15,7 @@ public class PGenEventHandler{
 	@SubscribeEvent(priority = EventPriority.NORMAL, receiveCanceled = true)
 	public void onPlayerTickEvent(PlayerTickEvent event){
 		if(!PGenMain.haveWarnedVersionOutOfDate && event.player.worldObj.isRemote && !PGenMain.versionChecker.isLatestVersion()){
-			ClickEvent versionCheckChatClickEvent = new ClickEvent(ClickEvent.Action.OPEN_URL, "https://minecraft.curseforge.com/projects/ParticleGenerator");
+			ClickEvent versionCheckChatClickEvent = new ClickEvent(ClickEvent.Action.OPEN_URL, "https://minecraft.curseforge.com/projects/Particle-Generator");
 			Style clickableChatStyle = new Style().setClickEvent(versionCheckChatClickEvent).setColor(TextFormatting.GREEN);
 			TextComponentString versionWarningChatComponent = new TextComponentString("Particle Generator is outdated! Click here to update to ");
 			TextComponentString versionComponent = new TextComponentString(PGenMain.versionChecker.getLatestVersionInfo().get(0));
